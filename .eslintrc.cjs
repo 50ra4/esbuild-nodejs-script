5945ac441147b91edb7b7c9b8981731b14234465
50ra4 <50ra4.dev.dev@gmail.com>
2024-01-19 19:58:10 +0900

    change: eslint extends rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     es2021: true,
   },
-  extends: ['standard-with-typescript', 'prettier'],
+  extends: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       env: {
