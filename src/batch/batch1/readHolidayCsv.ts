84a8224245ce5c9ce705b9e03cd36e04af19d647
50ra4 <50ra4.dev.dev@gmail.com>
2024-01-19 17:29:48 +0900

    fix: csv-parse/sync import

--- src/batch/batch1/readHolidayCsv.ts
+++ src/batch/batch1/readHolidayCsv.ts
@@ -1,8 +1,8 @@
 import { readFileSync } from 'node:fs';
-import { parse as csvParse } from 'csv-parse/sync';
+import { parse } from 'csv-parse/sync';
 
 export const readHolidayCsv = (filePath: string) => {
   const csvFile = readFileSync(filePath);
-  const [header, ...records]: string[][] = csvParse(csvFile);
+  const [header, ...records]: string[][] = parse(csvFile);
   return { header, records };
 };
