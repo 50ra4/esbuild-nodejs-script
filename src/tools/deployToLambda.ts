1152514668be5578daef693f6a1b013f429a7cc4
50ra4 <50ra4.dev.dev@gmail.com>
2024-01-21 17:48:02 +0900

    add: download code commit files

--- src/tools/deployToLambda.ts
+++ src/tools/deployToLambda.ts
@@ -1,4 +1,8 @@
-import { CodeCommitClient, GetFolderCommand } from '@aws-sdk/client-codecommit';
+import {
+  CodeCommitClient,
+  GetFolderCommand,
+  GetFileCommand,
+} from '@aws-sdk/client-codecommit';
 import { fetchCodeCommitFilePathsRecursively } from './fetchCodeCommitFilePathsRecursively';
 import { isNonNullable } from '@/utils/typeGuard';
 
@@ -9,31 +13,48 @@ export const deployToLambda = async () => {
   const codeCommitClient = new CodeCommitClient({ region: 'ap-northeast-1' });
 
   try {
-    // 直下からディレクトリの一覧を取得する
+    // 直下からfunctionsのフォルダ一覧を取得する
     const { subFolders = [] } = await codeCommitClient.send(
       new GetFolderCommand({
-        folderPath: 'edge/functions',
+        folderPath: rootFolderPath,
         repositoryName,
       }),
     );
-
     if (!subFolders.length) {
       throw new Error(`not found functions in folder(${rootFolderPath}).`);
     }
 
+    // フォルダーごとに再帰的にフォルダの一覧を取得する
     const targetFunctions = await Promise.all(
       subFolders
         .map((folder) => folder.absolutePath)
         .filter(isNonNullable)
         .map(async (folderPath) => {
-          const files = await fetchCodeCommitFilePathsRecursively({
+          const filePaths = await fetchCodeCommitFilePathsRecursively({
             codeCommitClient,
             folderPath,
             repositoryName,
           });
-          return { name: 'TODO: function-name', files };
+          return { name: 'TODO: function-name', filePaths };
         }),
     );
+
+    for (const { name, filePaths } of targetFunctions) {
+      // function-name（フォルダ）ごとにファイルの一覧を取得する
+      const results = await Promise.all(
+        filePaths.map((filePath) =>
+          codeCommitClient.send(
+            new GetFileCommand({ repositoryName, filePath }),
+          ),
+        ),
+      );
+
+      // TODO: 後で消す
+      console.log(
+        `[${name}] fetched files`,
+        results.map((v) => v.filePath),
+      );
+    }
 
     console.log('succeeded deployToLambda.', targetFunctions);
   } catch (error) {
